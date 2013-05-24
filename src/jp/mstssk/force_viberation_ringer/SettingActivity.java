63e929a0057e3ee3067d730a782bbf4c1fa420e3
mstssk <mstssk+github@gmail.com>
2013-05-24 10:47:07 +0900

    fixed #1 Switch on Viberation, when the mode is Silent.

--- src/jp/mstssk/force_viberation_ringer/SettingActivity.java
+++ src/jp/mstssk/force_viberation_ringer/SettingActivity.java
@@ -1,12 +1,15 @@
 package jp.mstssk.force_viberation_ringer;
 
+import jp.mstssk.force_viberation_ringer.AudioController.RingerMode;
 import android.app.Activity;
+import android.media.AudioManager;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.SystemService;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 /**
@@ -18,18 +21,37 @@ import com.googlecode.androidannotations.annotations.ViewById;
 @EActivity(R.layout.setting)
 public class SettingActivity extends Activity {
 
+	@SystemService
+	AudioManager audioManager;
+	private AudioController audioController;
+	private PackageController packageController;
+
 	@ViewById(R.id.forceViberation)
 	CheckBox forceViberation;
 
 	@AfterViews
-	void setup() {
-		final PackageController packageController = new PackageController(this);
-		this.forceViberation.setChecked(packageController.isReceiverStateEnabled(VolumeChangedReceiver_.class));
+	void setupInjecties() {
+		this.audioController = new AudioController(this.audioManager);
+		this.packageController = new PackageController(this);
+	}
+
+	@AfterViews
+	void setupCheckbox() {
+		this.forceViberation.setChecked(this.packageController.isReceiverStateEnabled(VolumeChangedReceiver_.class));
 		this.forceViberation.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(final CompoundButton buttonView, final boolean checked) {
-				packageController.setReceiverStateEnabled(VolumeChangedReceiver_.class, checked);
+				SettingActivity.this.switchForceViberationEnabled(checked);
 			}
 		});
+	}
+
+	private void switchForceViberationEnabled(final boolean enabled) {
+		this.packageController.setReceiverStateEnabled(VolumeChangedReceiver_.class, enabled);
+
+		// Switch on Viberation, when the mode is Silent.
+		if (this.audioController.getRingerMode() == RingerMode.SILENT) {
+			this.audioController.forceViberation();
+		}
 	}
 }
